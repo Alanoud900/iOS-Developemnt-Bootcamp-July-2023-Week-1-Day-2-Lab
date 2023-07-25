c82bbb6e8e694d2a54af0cbdd9f95d02a00597a2
Alanoud900 <140309909+Alanoud900@users.noreply.github.com>
2023-07-25 15:22:03 +0300

    Add files via upload

---
+++ Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+//Task 1
+var score=5
+score+=10
+print(score)
+//Task 2
+if (score > 9){
+    print("Pass")
+}
+else{
+        "Fail"
+    }
+//Task 3
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday"]
+for item in days {
+    print(item)
+}
+print("Array:\(days)")
+
+ //Task 4
+let fruits = ["apple": 3," orange":6,"Kiwi":4,"banana":2]
+for (fruitname, quantity) in fruits {
+    print("\(fruitname): \(quantity)")
+}
+//Task 5
+let name="alanoud"
+
+print("welcome",name)
